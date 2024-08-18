436627ed3ba8896f01f99e6c92de2dd2ea39a3c0
david <hoffi3089@gmail.com>
2024-08-17 21:43:09 -0700

    first commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,84 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+  UsePipes,
+  ValidationPipe,
+  ParseIntPipe,
+  UseGuards,
+  Logger,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { TaskStatus } from './task-status.enum';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetFilterTaskDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/tasks-status-validation.pipe';
+import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/auth/user.entity';
+import { GetUser } from 'src/auth/get-user.decorator';
+
+@Controller('tasks')
+@UseGuards(AuthGuard())
+export class TasksController {
+  private logger = new Logger('TasksController');
+
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getTasks(
+    @Query(ValidationPipe) filterDto: GetFilterTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${
+        user.username
+      }" retreving all the tasks. Filters : ${JSON.stringify(filterDto)}`,
+    );
+    return this.taskService.getTasks(filterDto, user);
+  }
+
+  @Get('/:id')
+  getTaskById(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.taskService.getTaskById(id, user);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new Task. Data : ${JSON.stringify(
+        createTaskDto,
+      )}`,
+    );
+    return this.taskService.createTask(createTaskDto, user);
+  }
+
+  @Delete('/:id')
+  deleteTask(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.taskService.deleteTask(id, user);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.taskService.updateTaskSatus(id, status, user);
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetFilterTaskDto } from './dto/get-tasks-filter.dto';
+import { TaskRepository } from './task.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
+import { User } from 'src/auth/user.entity';
+
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository,
+  ) {}
+
+  async getTasks(filterDto: GetFilterTaskDto, user: User): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto, user);
+  }
+
+  async getTaskById(id: number, user: User): Promise<Task> {
+    const found = await this.taskRepository.findOne({
+      where: { id, userId: user.id },
+    });
+
+    if (!found) {
+      throw new NotFoundException(`Task with id "${id}" not found`);
+    }
+
+    return found;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto, user);
+  }
+
+  async deleteTask(id: number, user: User): Promise<void> {
+    const result = this.taskRepository.delete({ id, userId: user.id });
+
+    if ((await result).affected == 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+  }
+
+  async updateTaskSatus(
+    id: number,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = this.getTaskById(id, user);
+    (await task).status = status;
+    (await task).save();
+
+    return task;
+  }
+}
